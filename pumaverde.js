c9240545b32d5fb42621319826a88abb5a6b7d52
Santiago Hugo <santiagohugo702@gmail.com>
2023-08-02 22:14:03 -0300

    se agrego slide para cada producto, correcion de textos y responsive!

---
+++ pumaverde.js
@@ -0,0 +1,22 @@
+const imagenes = ["./img/puma verde.jpg", "./img/aftertattoo2.jpg"];
+let indice = 0;
+const sliderImg = document.getElementById("sliderImg");
+const anteriorBtn = document.getElementById("anteriorBtnimg");
+const siguienteBtn = document.getElementById("siguienteBtnimg");
+
+function cambiarImagen(indice) {
+    sliderImg.src = imagenes[indice];
+    indice = (indice + 1) % imagenes.length;
+}
+
+anteriorBtn.addEventListener("click", function() {
+    indice = (indice - 1 + imagenes.length) % imagenes.length;
+    cambiarImagen(indice);
+});
+
+siguienteBtn.addEventListener("click", function() {
+    indice = (indice + 1) % imagenes.length;
+    cambiarImagen(indice);
+});
+
+
